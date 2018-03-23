28c00fea2205322bb7ba175e02f2018e1fe36e7e
Bernard Duggan <bernard@hippware.com>
2018-03-24 10:28:02 +1100

    Fix dev DB config

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,6 @@
 use Mix.Config
 
-config :wocky_db_watcher, Wocky.Repo, database: "wocky_dev"
+config :wocky_db_watcher, :db,
+  database: "wocky_dev"
 
 config :wocky_db_watcher, backend: WockyDBWatcher.Backend.Direct
